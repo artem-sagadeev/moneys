47e43ae6a7ea0f6a6d4de6dda38ee0874ed4dc06
artem-sagadeev <sagadeev.artem2001@gmail.com>
2023-05-14 17:15:00 +0300

    add new project

---
+++ src/RegularOperations/Entities/RegularPayment.cs
@@ -0,0 +1,12 @@
+﻿using Common.Interfaces;
+
+namespace RegularOperations.Entities;
+
+public class RegularPayment : IEntity
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public bool IsAutoExecution { get; set; }
+}
